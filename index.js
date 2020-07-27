ff831918cc661992e13217a48e2b109a516cfc99
Muhaimin Jobayer <mahmudmuhaimin@yahoo.com>
2020-07-27 03:23:19 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// put your own value below!
+const apiKey = 'QQjaWKuPIQ6Ua0vtekTxwjy9azee848dOEeY07aw'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson, maxResults) {
+  // if there are previous results, remove them
+  console.log(responseJson);
+  $('#results-list').empty();
+  // iterate through the items array
+  for (let i = 0; i < responseJson.data.length && i < maxResults; i++){
+    // for each video object in the items 
+    //array, add a list item to the results 
+    //list with the video title, description,
+    //and thumbnail
+    $('#results-list').append(
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <p>Website: <a href='${responseJson.data[i].url}'>${responseJson.data[i].url}</a></p>
+      </li>`
+    )};
+  //display the results section  
+  $('#results').removeClass('hidden');
+};
+
+function getParks(query, maxResults=10) {
+  const params = {
+    stateCode: query,
+    maxResults,
+    api_key: apiKey,
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson, maxResults))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getParks(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
